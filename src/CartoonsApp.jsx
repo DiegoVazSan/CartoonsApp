ce498e20dc3c80f0d2b11aac91de2d966dc8f841
Diego Vazquez Sanchez <diegov@Diegos-MacBook-Pro.local>
2024-11-22 15:43:40 -0600

    AddCategory Component

--- src/CartoonsApp.jsx
+++ src/CartoonsApp.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const CartoonsApp = () => {
-    return(
+
+    const [categories, setCategories] = useState(['One Punch', 'DragonBall']);
+
+    const onAddCategory = () => {
+        setCategories([...categories, 'Nabruto']);
+    }
+
+    return (
         <>
-            <h1>CartoonsApp</h1>
+            <h1>Cartoons App</h1>
+
+            <AddCategory />
+
+            <button onClick={ onAddCategory }>Add</button>
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { CartoonsApp } from './CartoonsApp';
-
+import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CartoonsApp />
   </StrictMode>,
-)
+);
