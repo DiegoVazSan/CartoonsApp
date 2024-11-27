4d3f3b2b43bce7b3341b5ed139800801b64cc91d
Diego Vazquez Sanchez <diegov@Diegos-MacBook-Pro.local>
2024-11-26 18:45:28 -0600

    Card showing the GIF

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,26 +1,30 @@
 import { useEffect, useState } from 'react';
 import { getGigfs } from '../helpers/getGifs'
+import { GiftItem } from './GiftItem';
 
 export const GiftGrid = ({ category }) => {
 
     const [images, setImages] = useState([]);
 
-    useEffect( () => {
+    useEffect(() => {
         getGigfs(category)
-        .then( newImages => setImages(newImages) );
+            .then(newImages => setImages(newImages));
 
     }, [])
 
     return (
         <>
             <h3> {category} </h3>
-            <ol>
+            <div className='card-grid'>
                 {
-                    images.map( ({id, title}) => (
-                        <li key={id}>{ title }</li>
+                    images.map(( img ) => (
+                        <GiftItem
+                        key={ img.id } 
+                        { ...img }
+                        />
                     ))
                 }
-            </ol>
+            </div>
         </>
     )
 }
---
+++ src/components/GiftItem.jsx
@@ -0,0 +1,12 @@
+export const GiftItem = ({title, url }) => {
+    
+    console.log(title)
+    console.log(url)
+
+    return (
+        <div className="card">
+            <img src={url} alt={title} />
+            <p>{ title }</p>
+        </div>
+    )
+}
